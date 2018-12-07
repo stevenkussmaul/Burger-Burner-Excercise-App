bce96ec026193396207057fb8c8b435d15f271ce
Steven Kussmaul <steve@Stevens-MacBook-Air.local>
2018-12-07 09:03:16 -0500

    api call changes

--- app/components/results.js
+++ app/components/results.js
@@ -14,18 +14,6 @@ const results = {
 
         </section>
 
-        <section class="distance2">
-
-            <svg viewBox="0 0 500 500">
-                <path class="curve-path" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-                    <text class="curve-text" width="500">
-                        <textPath xlink:href="#curve">
-                            Dangerous Curves Ahead!
-                        </textPath>
-                    </text>
-            </svg>
-        </section>
-
         <section class="movement-container">
             <section class="skyline-container">
                 <section class="skyline-background">
