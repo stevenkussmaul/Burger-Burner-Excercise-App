3f56f1adb86953de9eb053321c2f49904bf3b876
christina <cmmiller.406@gmail.com>
2018-12-11 10:28:16 -0500

    updated port

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const app = express();
 const food = require("./routes.js");
-const port = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(express.static("./"));
 app.use("/",food);
 
-app.listen(3000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
     console.log("Server is on port 3000");
 });
