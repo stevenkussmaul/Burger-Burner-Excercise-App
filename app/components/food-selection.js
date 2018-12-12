f97ef017c8c6ad02f6cb922382887850c76c576e
84tigers <ahyska@gmail.com>
2018-12-12 15:12:16 -0500

    garbage commit just to update branch

--- app/components/food-selection.js
+++ app/components/food-selection.js
@@ -4,7 +4,7 @@ const foodSelection = {
     template: `
 
     <section ng-click= "$ctrl.homePage();" class="title">BURGER BURNER</section>
-
+    
     <h2 class="click-hint">Click to add / delete items</h2>
 
     <button class="go-btn" ng-show="$ctrl.buttonShow" ng-click="$ctrl.goActivity();">I'm Full</button>
@@ -28,7 +28,11 @@ const foodSelection = {
         </section>
       
     </section>
+    <section landing-animation class="landing--container">
+    <?xml version="1.0" standalone="no"?><svg class="flame--upper" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 375 812" preserveAspectRatio="none" width="375" height="812"><defs><clipPath id="_clipPath_oyAhxijvcyQw9XHLW2oajDuHLX0lvUF0"><rect width="375" height="812"/></clipPath></defs><g clip-path="url(#_clipPath_oyAhxijvcyQw9XHLW2oajDuHLX0lvUF0)"><g style="isolation:isolate"><path class="flame flame--upper__path" d=" M 156.36 600 C 148.238 525.839 80.667 479.333 120 440 C 80.667 441.333 51.333 540 81.36 600 C 75.256 574.584 32 571.333 44.52 540.64 C 26 551.333 12.667 561.333 0 600 L 0 812.25 L 375 811.875 L 375 600 C 341.272 596.781 326.322 576.844 330.12 540.16 C 300.667 571.333 301.333 580 293.4 600 C 284.244 525.906 218.667 530 256.44 441.92 C 234.679 486.601 205.333 472.667 217.68 600 C 187.333 481.333 240 375 187.5 350 C 213.333 411.333 100.667 419.333 156.36 600 Z " fill="#DD1A08" vector-effect="non-scaling-stroke" stroke-width="1" stroke="#DD1A08" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/></g></g></svg>
+    <?xml version="1.0" standalone="no"?><svg class="flame--lower" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 375 812" preserveAspectRatio="none" width="375" height="812"><defs><clipPath id="_clipPath_oyAhxijvcyQw9XHLW2oajDuHLX0lvUF0"><rect width="375" height="812"/></clipPath></defs><g clip-path="url(#_clipPath_oyAhxijvcyQw9XHLW2oajDuHLX0lvUF0)"><g style="isolation:isolate"><path class="flame flame--lower__path" d=" M 156.36 600 C 148.238 525.839 80.667 479.333 120 440 C 80.667 441.333 51.333 540 81.36 600 C 75.256 574.584 32 571.333 44.52 540.64 C 26 551.333 12.667 561.333 0 600 L 0 812.25 L 375 811.875 L 375 600 C 341.272 596.781 326.322 576.844 330.12 540.16 C 300.667 571.333 301.333 580 293.4 600 C 284.244 525.906 218.667 530 256.44 441.92 C 234.679 486.601 205.333 472.667 217.68 600 C 187.333 481.333 240 375 187.5 350 C 213.333 411.333 100.667 419.333 156.36 600 Z " fill="#ff0000" vector-effect="non-scaling-stroke" stroke-width="1" stroke="#ff0000" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/></g></g></svg>
 
+    </section>
     `,
     bindings: [],
     controller: ["Service", "$location", "$timeout", function (Service, $location, $timeout) {
